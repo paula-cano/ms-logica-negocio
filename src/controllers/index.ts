ee8eacf5f6c5962ed6e8af940478f58467eb0d23
paula-cano <paula.14canob@gmail.com>
2023-03-25 18:38:02 -0500

    primer commit, creacion de modelos y relacion, enlace con base de datos de mysql

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './ping.controller';
+export * from './cliente-venta.controller';
+export * from './venta-cliente.controller';
+export * from './producto-venta.controller';
+export * from './venta-producto.controller';
