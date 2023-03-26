dd73b054d610a6e25ff7b818545afb74b1e4791a
paula-cano <paula.14canob@gmail.com>
2023-03-25 21:28:30 -0500

    creacion de relaciones y controles de cliente, producto y venta. creacion de llave foranea en el modelo de venta

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,6 @@ export * from './cliente-venta.controller';
 export * from './venta-cliente.controller';
 export * from './producto-venta.controller';
 export * from './venta-producto.controller';
+export * from './cliente.controller';
+export * from './producto.controller';
+export * from './venta.controller';
